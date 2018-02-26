bc0f627409cb44e1e55a69502241559f966fabd2
Rani Shah <rani76@gmail.com>
2018-02-26 17:14:52 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,7 +3,7 @@ return num1 + num2
 end
 
 def subtraction(num1, num2)
-puts num1 - num2
+return num1 - num2
 end
 
 def division(num1, num2)
@@ -19,7 +19,7 @@ return num1 % num2
 end
 
 def square_root(num)
-return math.sqrt(num)
+return Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
